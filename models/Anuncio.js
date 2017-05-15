01cc814cab911bfa9389f52879d92aa72107dc9c
Carlos <thehandyowl.io@gmail.com>
2017-05-16 01:12:22 +0200

    Activamos filtros en el modelo anuncios

--- models/Anuncio.js
+++ models/Anuncio.js
@@ -10,6 +10,17 @@ const anuncioSchema = mongoose.Schema({
     tags: [String]
 });
 
+anuncioSchema.statics.list = function (filter, limit, skip, sort, fields, callback) {
+
+    const query = Anuncio.find(filter);
+    query.limit(limit);
+    query.skip(skip);
+    query.sort(sort);
+    query.select(fields);
+    query.exec(callback);
+
+};
+
 var Anuncio = mongoose.model( 'Anuncio', anuncioSchema );
 
 module.exports = Anuncio;
